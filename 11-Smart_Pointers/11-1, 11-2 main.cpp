f543ff4cc76409c5a226b01967051b41864c238b
oldmanclancy <wldfngrs@gmail.com>
2022-05-09 10:44:10 +0100

    update

---
+++ 11-Smart_Pointers/11-1, 11-2 main.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <memory>
+
+using FileGuard = std::shared_ptr<FILE>;
+
+void say_hello(FileGuard file) {
+	fprintf(file.get(), "HELLO DAVE");
+}
+
+int main() {
+	auto file = fopen("HAL9000", "w");
+	if(!file) return errno;
+	FileGuard file_guard{ file };
+	say_hello(file_guard);
+	say_hello(file_guard); //additional call to say_hello
+	return 0;
+}
