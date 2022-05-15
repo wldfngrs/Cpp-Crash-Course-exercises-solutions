21eb220040607a8d77529f6890b5069f13e26c5d
oldmanclancy <wldfngrs@gmail.com>
2022-05-15 20:36:52 +0100

    commit

--- 12-Utilities/12-2 main.cpp
+++ 12-Utilities/12-2 main.cpp
@@ -2,18 +2,22 @@
 #include <cstdio>
 #include <iostream>
 
-int main() {
-	const int passLength = 12;
-	char characters[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
+void generatePass(int pLength, char* chars, int passNum) {
 	std::random_device rd_engine{};
-	std::uniform_int_distribution dist{0,61};
-	int generateNumber = 9;
-	for (int i = 0; i < generateNumber; i++) {
+	std::uniform_int_distribution dist{ 0,61 };
+	for (int i = 0; i < passNum; i++) {
 		std::cout << " password_" << i << " : ";
-		for (int j = 0; j < passLength; j++) {
+		for (int j = 0; j < pLength; j++) {
 			size_t index = dist(rd_engine);
-			std::cout << characters[index];
+			std::cout << chars[index];
 		}
 		std::cout << "\n";
 	}
+}
+
+int main() {
+	const int passLength = 12;
+	char characters[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
+	int passwordsToGenerate = 9;
+	generatePass(passLength, characters, passwordsToGenerate);
 }
